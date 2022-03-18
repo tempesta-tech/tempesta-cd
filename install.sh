bae34cb1949aef15aea8a92f92689b1a72001e64
pale_emperror <beloglazmv@gmail.com>
2022-03-19 02:28:29 +0300

    Complete to work nexus install distro

---
+++ install.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+dir=$(dirname "$0")
+
+echo "Dir: $dir"
+apt install openjdk-8-jdk -y
+
+useradd -M -d /opt/nexus -s /bin/bash -r nexus
+echo "nexus   ALL=(ALL)       NOPASSWD: ALL" > /etc/sudoers.d/nexus
+
+# Extract /opt/nexus
+tar -xzvf $dir/nexus.tar.gz -C /opt/
+# Extract /opt/sonatype-work
+tar -xzvf $dir/sonatype-work.tar.gz -C /opt/
+
+chown -R nexus: /opt/nexus
+
+# Create systemd unit
+cp nexus.service /etc/systemd/system/nexus.service
+
+systemctl daemon-reload
+systemctl restart nexus
